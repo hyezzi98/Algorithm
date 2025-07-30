747ff3bc9feb49915cdf42ac85f90882108da900
🎀혜찌🎀 <73926423+hyezzi98@users.noreply.github.com>
2025-07-30 21:48:47 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+using namespace std;
+
+void recursive(int depth, int array[], bool visited[], int start);
+
+int n, m;
+vector<int> result;
+set<vector<int>> allResult;
+
+int main() {
+    cin >> n >> m;
+    int array[n];
+    bool visited[n] = {false};
+    for (int i = 0 ; i < n; i++) {
+        cin >> array[i];
+    }
+    sort(array, array + n);
+    recursive(0, array, visited, 0);
+    for (const auto &all : allResult) {
+        for (int num : all) {
+            cout << num << " ";
+        }
+        cout << "\n";
+    }
+}
+
+void recursive(int depth, int array[], bool visited[], int start) {
+    // basis part
+    if (depth == m) {
+        // 여기서 벡터에 저장
+        allResult.insert(result);
+        return;
+    }
+
+    // inductive part
+    for (int i = start; i < n; i++) {
+        if (!visited[i]) {
+            int number = array[i];
+            visited[i] = true;
+            result.push_back(number);
+            recursive(depth + 1, array, visited, i + 1);
+            visited[i] = false;
+            result.pop_back();
+        }
+    }
+}
