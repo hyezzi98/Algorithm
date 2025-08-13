3c81a3268dd7b33f3da50f44011b2381dacbf77f
🎀혜찌🎀 <73926423+hyezzi98@users.noreply.github.com>
2025-08-13 22:55:33 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 104 ms, Memory: 12360 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -4,37 +4,33 @@ using namespace std;
 vector<vector<int>> tree;
 vector<int> parent;
 vector<bool> visited;
-void dfs(int cur); 
+int n;
+
+void dfs(int cur) {
+    visited[cur] = true;
+    for (auto next : tree[cur]) {
+        if (!visited[next]) {
+            parent[next] = cur;
+            dfs(next);
+        }
+    }
+}
 
 int main() {
-    int n;
     cin >> n;
-
-    tree = vector<vector<int>>(n + 1);
-    parent = vector<int>(n + 1);
-    visited = vector<bool>(n + 1);
-    
+    tree.resize(n + 1);
+    parent.resize(n + 1);
+    visited.resize(n + 1, false);
     for (int i = 0; i < n - 1; i++) {
-        int a,b;
+        int a, b;
         cin >> a >> b;
         tree[a].push_back(b);
         tree[b].push_back(a);
     }
 
     dfs(1);
-    for (int i = 2; i <= n; i++) {
-        cout << parent[i] << "\n";
-    }
-
-}
-
-void dfs(int cur) {
-    visited[cur] = true;
-    for (int next : tree[cur]) {
-        if (!visited[next]) {
-            parent[next] = cur;
-            dfs(next);
-        }
+    for (int i = 2; i < n + 1; i++) {
+        cout << parent[i] <<"\n";
     }
     
 }
