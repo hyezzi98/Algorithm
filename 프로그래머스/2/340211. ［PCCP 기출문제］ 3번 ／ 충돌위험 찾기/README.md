35ddb2e86fdf56ee31144ca5305aad5dfe379037
🎀혜찌🎀 <73926423+hyezzi98@users.noreply.github.com>
2025-08-17 15:32:34 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 172.82 ms, Memory: 15.7 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/README.md
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] [PCCP 기출문제] 3번 / 충돌위험 찾기 - 340211 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340211?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340211) 
 
 ### 성능 요약
 
-메모리: 15.7 MB, 시간: 176.60 ms
+메모리: 15.7 MB, 시간: 172.82 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 16일 17:39:34
+2025년 08월 17일 15:32:32
 
 ### 문제 설명
 
--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.cpp
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.cpp
@@ -1,9 +1,56 @@
+#include <iostream>
 #include <string>
 #include <vector>
-
+#include <map>
 using namespace std;
+
 
 int solution(vector<vector<int>> points, vector<vector<int>> routes) {
     int answer = 0;
+    int robots = routes.size();
+    map<int, vector<pair<int,int>>> path;
+    
+    // 각 위치에 로봇 배치
+    for (int i = 0; i < robots; i++) {
+        int curY, curX;
+        int targetY, targetX;
+        curY = points[routes[i][0] - 1][0];
+        curX = points[routes[i][0] - 1][1];
+        path[0].push_back({curY, curX});
+        int time = 0;
+        for (int j = 1; j < routes[i].size(); j++) {
+            targetY = points[routes[i][j] - 1][0];
+            targetX = points[routes[i][j] - 1][1];
+            
+            // Y먼저 이동 후 X 이동시키기
+            while (curY != targetY) {
+                if (curY < targetY) curY++;
+                else curY--;
+                time++;
+                path[time].push_back({curY, curX});
+                }
+            while (curX != targetX) {
+                if (curX < targetX) curX++;
+                else curX--;
+                time++;
+                path[time].push_back({curY, curX});
+                }
+            }
+        }
+        // 충돌체크
+        for (auto &p : path) {
+            // 같은 시간에 같은 좌표 있는지 체크
+            map<pair<int,int>, int> counter;
+            for (auto &coord : p.second) {
+                counter[coord]++;
+            }
+            // 여러 개 있는지 체크
+            for (auto &c : counter) {
+                if (c.second > 1) answer++;
+            }
+    // for (auto r: robotPos) {
+    //     cout << r.first << " " << r.second << "\n";
+    // }
+    }
     return answer;
 }
